e4b255a065b1a86e7b7a0cfd600c088c9ae376bf
Klaus Ening <klaus.ening@sintef.no>
2020-12-01 18:09:35 +0100

    starting trafo, units, paths

--- examples/run_on_camera_sequence.cpp
+++ examples/run_on_camera_sequence.cpp
@@ -16,10 +16,10 @@
 
 int main() {
   // Change accordingly
-  const std::string model_path{"/your/path/to/save/and/load/models/"};
+  const std::string model_path{"/home/klause/tmp/model/"};
 
   constexpr bool kSaveViewerImage = false;
-  const std::string viewer_save_path{"/your/path/to/save/viewer/images/"};
+  const std::string viewer_save_path{"/home/klause/tmp/images/"};
 
   // Set up tracker and renderer geometry
   auto tracker_ptr{std::make_shared<rbgt::Tracker>()};
@@ -36,15 +36,16 @@ int main() {
   tracker_ptr->AddViewer(viewer_ptr);
 
   // Set up body1 (change accordingly)
-  const std::string body1_geometry_path{"/your/path/to/body1.obj"};
+  const std::string body1_geometry_path{"/home/klause/tmp/obj/body1.obj"};
   rbgt::Transform3fA body1_geometry2body_pose{
       Eigen::Translation3f(0.0f, 0.0f, 0.0f)};
   rbgt::Transform3fA body1_world2body_pose;
-  body1_world2body_pose.matrix() << -0.845644f, -0.271396f, 0.459818f,
-      -0.0707055f, -0.533926f, 0.43576f, -0.724728f, 0.254323f, -0.00367445f,
-      -0.858296f, -0.513344f, 0.279658f, 0.0f, 0.0f, 0.0f, 1.0f;
+  body1_world2body_pose.matrix() << -0.845644f,   -0.271396f,  0.459818f,  0.0f,
+                                    -0.533926f,    0.43576f,  -0.724728f,  0.0f,
+                                    -0.00367445f, -0.858296f, -0.513344f,  2.0f,
+                                     0.0f,         0.0f,       0.0f,       1.0f;
   auto body1_ptr{std::make_shared<rbgt::Body>("body1", body1_geometry_path,
-                                              1.0f, true, true, 0.1f,
+                                              0.001f, true, true, 0.6f,
                                               body1_geometry2body_pose)};
   body1_ptr->set_world2body_pose(body1_world2body_pose);
   body1_ptr->set_occlusion_mask_id(1);
@@ -64,8 +65,9 @@ int main() {
                                   body1_model_ptr, camera_ptr);
   tracker_ptr->AddRegionModality(body1_region_modality_ptr);
 
+  
   // Set up body2 (change accordingly)
-  const std::string body2_geometry_path{"/your/path/to/body2.obj"};
+  const std::string body2_geometry_path{"/home/klause/tmp/obj/body2.obj"};
   rbgt::Transform3fA body2_geometry2body_pose{
       Eigen::Translation3f(0.0f, 0.0f, 0.0f)};
   rbgt::Transform3fA body2_world2body_pose;
@@ -73,7 +75,7 @@ int main() {
       0.108489f, -0.0319917f, 0.88242f, 0.469534f, -0.155606f, -0.00332271f,
       0.469629f, -0.882873f, 0.416361f, 0.0f, 0.0f, 0.0f, 1.0f;
   auto body2_ptr{std::make_shared<rbgt::Body>("body2", body2_geometry_path,
-                                              1.0f, true, true, 0.1f,
+                                              0.001f, true, true, 0.1f,
                                               body2_geometry2body_pose)};
   body2_ptr->set_world2body_pose(body2_world2body_pose);
   body2_ptr->set_occlusion_mask_id(2);
@@ -93,6 +95,7 @@ int main() {
                                   body2_model_ptr, camera_ptr);
   tracker_ptr->AddRegionModality(body2_region_modality_ptr);
 
+  
   // Set up occlusion mask renderer
   auto occlusion_mask_renderer_ptr{
       std::make_shared<rbgt::OcclusionMaskRenderer>()};
